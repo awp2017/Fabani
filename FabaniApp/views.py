395343e56885992f1de9f030c54c9ded378eaf03
madaluna <madalinamarin1996@gmail.com>
2018-01-12 23:22:53 +0200

    bla bla

--- FabaniApp/views.py
+++ FabaniApp/views.py
@@ -35,3 +35,14 @@ def login_view(request):
     context['form'] = form
     return render(request, 'login.html', context)
 
+def register_view(request):
+	if request.method == "GET":
+		form = EmployerRegisterForm()
+	elif request.method == "POST":
+		form = EmployerRegisterForm(request.POST)
+	if form.is_valid():
+   #user.Create....
+class employerProfile(DetailView):
+    template_name = 'employerProfile.html'
+   # model = Employer
+    # context_object_name = 'employer'
