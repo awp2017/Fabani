5970eb3c7e312fdd31d8db5031da7a32e7ab615b
gabrieluta <gabrieladobrovat@gmail.com>
2018-01-12 17:47:20 +0200

    Made Project and Comment models.

--- FabaniApp/models.py
+++ FabaniApp/models.py
@@ -4,3 +4,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=45)
+    # idEmployee = models.ManyToManyField(Employee, null=True)
+    # idEmployer = models.ForeignKey(Employer)
+    # idSkillList = models.ForeignKey(SkillList)
+    deadline = models.DateField()
+    payment = models.IntegerField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    # idProject = models.ForeignKey(Project)
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.textsss
