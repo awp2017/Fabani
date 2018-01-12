88ec0c9f9ee43c827016e0c826a9fdbf19ca9156
Robert Tanase <robert.tanase@cst-dev.com>
2018-01-12 18:02:59 +0200

    Skill Model done

--- FabaniApp/models.py
+++ FabaniApp/models.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+	name = models.CharField(max_length = 30, blanck = False)
+	proficiency = models.PositiveSmallIntegerField(choices=[
+            (1, "Beginner"),
+            (2, "Junior"),
+            (3, "Middle"),
+            (4, "Senior"),
+            (5, "Expert"),
+        ], default = 1)
+
+	def __str__(self):
+        return self.name
