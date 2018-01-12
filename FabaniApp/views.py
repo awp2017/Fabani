8a763dfddb5b98ffaa5a0348ab1c85409b5970ec
gabrieluta <gabrieladobrovat@gmail.com>
2018-01-12 16:32:45 +0200

    Added layout.

--- FabaniApp/views.py
+++ FabaniApp/views.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+import json
+
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
