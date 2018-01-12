b6eb95b0be1f41a5b0ffd402d11a2a7e44283894
Robert Tanase <robert.tanase@cst-dev.com>
2018-01-12 18:45:34 +0200

    Added Skill Template and Remove Skill Template

--- FabaniApp/models.py
+++ FabaniApp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Skill(models.Model):
 	name = models.CharField(max_length = 30, blank = False)
+	description = models.CharField(max_length = 200, blank = True)
 	proficiency = models.PositiveSmallIntegerField(choices=[
             (1, "Beginner"),
             (2, "Junior"),
@@ -12,6 +13,9 @@ class Skill(models.Model):
             (4, "Senior"),
             (5, "Expert"),
         ], default = 1)
+
+	created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return self.name
