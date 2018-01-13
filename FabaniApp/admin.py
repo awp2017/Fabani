c561bc3c76a82f024c31520665bd7e22830a4be7
gabrieluta <gabrieladobrovat@gmail.com>
2018-01-13 09:46:19 +0200

    Imported needed models for admin.py

--- FabaniApp/admin.py
+++ FabaniApp/admin.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from FabaniApp.models import Skill
+from FabaniApp.models import Project
+from FabaniApp.models import Comment
 
 # Register your models here.
 admin.site.register(Skill)
